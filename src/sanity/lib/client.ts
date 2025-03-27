45aba7b674849e507f6c6a8b35b73d95d1e74c53
sumanpatra03 <sumanpatra32003@gmail.com>
2025-03-27 12:15:54 +0530

    sanity

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,14 +1,8 @@
-import { createClient } from 'next-sanity'
-
-import { apiVersion, dataset, projectId } from '../env'
-
+import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId: "etshyk9u",
+  dataset: "production",
+
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
-
-
-
+});
